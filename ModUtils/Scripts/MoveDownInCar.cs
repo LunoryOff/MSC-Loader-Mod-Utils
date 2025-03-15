48b0034cc35ed3abc897b6c50f4f9ff78b0e24b5
Mechanic <madgun946@gmail.com>
2025-03-15 13:09:49 +0700

    MoveDownInCar

---
+++ ModUtils/Scripts/MoveDownInCar.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace ModUtils.Drivable
+{
+    
+    public class MoveDownInCar : MonoBehaviour
+    {
+        void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.name == "PLAYER")
+            {
+                other.gameObject.GetComponents<PlayMakerFSM>()[1].FsmVariables.GetFsmBool("PlayerInCar").Value = true;
+            }
+        }
+
+        void OnTriggerExit(Collider other)
+        {
+            if (other.gameObject.name == "PLAYER")
+            {
+                other.gameObject.GetComponents<PlayMakerFSM>()[1].FsmVariables.GetFsmBool("PlayerInCar").Value = false;
+            }
+        }
+    }
+}
