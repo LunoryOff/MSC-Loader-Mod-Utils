aec7abcc7f39a7a72c1e0feea20d1e40ea4d3888
Mongabox <lunarhinvery@gmail.com>
2025-03-15 10:00:36 +0300

    Update 1.1.5

--- Documentation.md
+++ Documentation.md
@@ -17,7 +17,7 @@
   - `ReturnObject()`: Return an object to the pool.
 
 ## `PlayerValues`
-- **Description**: Simplification of interction with player values.
+- **Description**: Simplification of interaction with player values.
 - **Variables**:
   - `Stress`: Changes player's stress.
   - `Fatigue`: Changes player's fatigue.
@@ -28,16 +28,22 @@
   - `Urine`: Changes player's urine.
   - `Dirtiness`: Changes player's dirtiness.
   - `Weight`: Changes player's weight.
-  - `KeyFerndale': Changes whether the player has a key to Ferndale.
-  - `KeyGifu': Changes whether the player has a key to Gifu.
-  - `KeyHome': Changes whether the player has a key to Home.
-  - `KeySatsuma': Changes whether the player has a key to Satsuma.
-  - `KeyRuscko': Changes whether the player has a key to Ruscko.
+  - `KeyFerndale`: Changes whether the player has a key to Ferndale.
+  - `KeyGifu`: Changes whether the player has a key to Gifu.
+  - `KeyHome`: Changes whether the player has a key to Home.
+  - `KeySatsuma`: Changes whether the player has a key to Satsuma.
+  - `KeyRuscko`: Changes whether the player has a key to Ruscko.
   - `CurrentVehicle`: Changes player's CurrentVehicle name.
   - `GUIinteraction`: Changes GUI Interaction.
   - `GUIsubtitle`: Changes GUI Subtitle.
   - `GUIGear`: Changes GUI Gear.
   - `PlayerStop`: Changes player's stop.
-  - `Seat`: Changes player's is sitting or no.
+  - `Seat`: Changes player's sitting status.
+
+## `MoveDownInCar`
+- **Description**: This script manages the player's state when entering or exiting a vehicle. It sets a boolean variable indicating whether the player is in the car.
+- **Methods**:
+  - `OnTriggerEnter(Collider other)`: Triggered when another collider enters the trigger collider attached to the GameObject. If the collider belongs to the player, it sets the `PlayerInCar` variable to `true`.
+  - `OnTriggerExit(Collider other)`: Triggered when another collider exits the trigger collider attached to the GameObject. If the collider belongs to the player, it sets the `PlayerInCar` variable to `false`.
 
 [View full examples](ExampleMod/UtilsExample.cs)
--- ModUtils/Scripts/PlayerValues.cs
+++ ModUtils/Scripts/PlayerValues.cs
@@ -37,139 +37,139 @@ namespace ModUtils
 
         public static float Stress
         {
-            
+            get => Framework_Float(Values[0]).Value;
             set => Framework_Float(Values[0]).Value = value;
         }
 
         public static float Fatigue
         {
-            
+            get => Framework_Float(Values[1]).Value;
             set => Framework_Float(Values[1]).Value = value;
         }
 
         public static float FatigueRate
         {
-            
+            get => Framework_Float(Values[2]).Value;
             set => Framework_Float(Values[2]).Value = value;
         }
 
         public static float Hunger
         {
-            
+            get => Framework_Float(Values[3]).Value;
             set => Framework_Float(Values[3]).Value = value;
         }
 
         public static float Thirst
         {
-            
+            get => Framework_Float(Values[4]).Value;
             set => Framework_Float(Values[4]).Value = value;
         }
 
         public static float DrunkEffect
         {
-            
+            get => Framework_Float(Values[5]).Value;
             set => Framework_Float(Values[5]).Value = value;
         }
 
         public static float Money
         {
-            
+            get => Framework_Float(Values[6]).Value;
             set => Framework_Float(Values[6]).Value = value;
         }
 
         public static float Urine
         {
-           
+            get => Framework_Float(Values[7]).Value;
             set => Framework_Float(Values[7]).Value = value;
         }
 
         public static float Dirtiness
         {
-           
+            get => Framework_Float(Values[8]).Value;
             set => Framework_Float(Values[8]).Value = value;
         }
 
         public static float Weight
         {
-            
+            get => Framework_Float(Values[9]).Value;
             set => Framework_Float(Values[9]).Value = value;
         }
 
         public static int CurrentСigarettes
         {
-            
+            get => Framework_Int(Values[10]).Value;
             set => Framework_Int(Values[10]).Value = value;
         }
 
         public static int KeyFerndale
         {
-            
+            get => Framework_Int(Values[11]).Value;
             set => Framework_Int(Values[11]).Value = value;
         }
 
         public static int KeyGifu
         {
-            
+            get => Framework_Int(Values[12]).Value;
             set => Framework_Int(Values[12]).Value = value;
         }
 
         public static int KeyHayosiko
         {
-            
+            get => Framework_Int(Values[13]).Value;
             set => Framework_Int(Values[13]).Value = value;
         }
 
         public static int KeyHome
         {
-            
+            get => Framework_Int(Values[14]).Value;
             set => Framework_Int(Values[14]).Value = value;
         }
 
         public static int KeySatsuma
         {
-            
+            get => Framework_Int(Values[15]).Value;
             set => Framework_Int(Values[15]).Value = value;
         }
 
         public static int KeyRuscko
         {
-            
+            get => Framework_Int(Values[16]).Value;
             set => Framework_Int(Values[16]).Value = value;
         }
 
         public static string CurrentVehicle
         {
-            
+            get => Framework_String(Values[17]).Value;
             set => Framework_String(Values[17]).Value = value;
         }
 
         public static string GUIinteraction
         {
-            
+            get => Framework_String(Values[18]).Value;
             set => Framework_String(Values[18]).Value = value;
         }
 
         public static string GUIsubtitle
         {
-            
+            get => Framework_String(Values[19]).Value;
             set => Framework_String(Values[19]).Value = value;
         }
 
         public static string GUIGear
         {
-            
+            get => Framework_String(Values[20]).Value;
             set => Framework_String(Values[20]).Value = value;
         }
 
         public static bool PlayerStop
         {
-            
+            get => Framework_Bool(Values[21]).Value;
             set => Framework_Bool(Values[21]).Value = value;
         }
 
         public static bool Seat
         {
-            
+            get => Framework_Bool(Values[22]).Value;
             set => Framework_Bool(Values[22]).Value = value;
         }
 
